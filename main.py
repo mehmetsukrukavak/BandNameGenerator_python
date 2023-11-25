b8d468893abeb6172f60884fa9f8f39a1c4fd715
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-25 16:50:21 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome To Band Name Generator.")
+cityName = input("What's name of the city you grew up in? ")
+petName = input("What's your pet's name? ")
+
+print(f"Your band name could be {cityName} {petName}")
